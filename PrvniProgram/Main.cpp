3359fc49947830edf35866ae62e943e7b185eb6c
VlcekDaniel <VlcekDaaniel@gmail.com>
2020-10-04 23:06:42 +0200

    PrvniProgram

--- PrvniProgram/Main.cpp
+++ PrvniProgram/Main.cpp
@@ -2,9 +2,26 @@
 
 using namespace std;
 
-main(){
+int main(){
 
+	int a,b,c;
 
+	cout << "Zadejte stranu A: "<< endl;
+	cin >> a;
 
+	cout << "Zadejte stranu B: " << endl;
+	cin >> b;
+
+	cout << "Zadejte stranu C: " << endl;
+	cin >> c;
+
+	if (a + b > c && a + c > b && b + c > a)
+	{
+		cout << "Trojuhelnik lze sestrojit, obvod je " << a + b + c << "." << endl;
+	}
+	else
+	{
+		cout << "Trojuhelnik nelze sestrojit." << endl;
+	}
 	return 0;
 }
